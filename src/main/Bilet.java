7350ee6ab4611ea8df42e6f3eaac9fbf8344dc0e
Mihaila Valerio <mihaila.andrei95@gmail.com>
2017-03-29 10:50:01 +0300

    hashcode and equals

--- src/main/Bilet.java
+++ src/main/Bilet.java
@@ -4,9 +4,22 @@ import java.util.Vector;
 
 public class Bilet {
 
-  private Integer pret;
+  private Integer pret=1;
   private Reprezentatie myReprezentatie;
   private Loc myLoc;
+
+    @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Bilet bilet = (Bilet) o;
+
+    if (pret != null ? !pret.equals(bilet.pret) : bilet.pret != null) return false;
+    if (myReprezentatie != null ? !myReprezentatie.equals(bilet.myReprezentatie) : bilet.myReprezentatie != null)
+      return false;
+    return myLoc != null ? myLoc.equals(bilet.myLoc) : bilet.myLoc == null;
+  }
 
   public Integer getPret() {
     return pret;
