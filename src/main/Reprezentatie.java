76eea61eb9fd4073ea7d7d47cc01a75a773e0bcc
Mihaila Valerio <mihaila.andrei95@gmail.com>
2017-03-29 11:01:17 +0300

    update reprezentatie

--- src/main/Reprezentatie.java
+++ src/main/Reprezentatie.java
@@ -1,53 +1,33 @@
 package main;
 
+import java.util.List;
 import java.util.Vector;
 
 public class Reprezentatie {
 
+  private final Integer nrLocuri;
+
   private String data;
+
   private Integer ora;
+
   private Sala sala;
-  private Vector<Bilet>  bilete;
 
-  public String getData() {
-    return data;
-  }
+  private List<Bilet> bilete;
 
-  public void setData(String data) {
-    this.data = data;
-  }
 
-  public Integer getOra() {
-    return ora;
-  }
-
-  public void setOra(Integer ora) {
-    this.ora = ora;
-  }
-
-  public Sala getSala() {
-    return sala;
-  }
-
-  public void setSala(Sala sala) {
-    this.sala = sala;
-  }
-
-  public Integer getNrLocuri() {
-    return nrLocuri;
-  }
-
-  public void setNrLocuri(Integer nrLocuri) {
-    this.nrLocuri = nrLocuri;
-  }
-
-  public Vector<Bilet> getBilete() {
+  public List<Bilet> getBilete() {
     return bilete;
   }
 
-  public void setBilete(Vector<Bilet> bilete) {
+  public Reprezentatie(String data, Integer ora, Sala sala, Vector<Bilet> bilete, Integer nrLocuri) {
+    this.data = data;
+    this.ora = ora;
+    this.sala = sala;
     this.bilete = bilete;
+    this.nrLocuri = nrLocuri;
   }
+
   public void addBilet(Bilet bilet){
     this.bilete.equals(bilet);
   }
@@ -58,13 +38,5 @@ public class Reprezentatie {
     }
     return 0;
   }
-  public Reprezentatie(String data, Integer ora, Sala sala, Vector<Bilet> bilete, Integer nrLocuri) {
-    this.data = data;
-    this.ora = ora;
-    this.sala = sala;
-    this.bilete = bilete;
-    this.nrLocuri = nrLocuri;
-  }
-  private Integer nrLocuri;
 
 }
